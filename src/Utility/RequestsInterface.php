b09c02a6c4696a0b6f2e3294a54aa1ed23555e8d
Victor Rosu <mrred85@gmail.com>
2018-08-10 00:14:39 +0300

    Header comment update

--- src/Utility/RequestsInterface.php
+++ src/Utility/RequestsInterface.php
@@ -2,8 +2,7 @@
 /**
  * Requests Utility Interface
  *
- * @package Requests
- * @link https://github.com/mrred85
+ * @link https://github.com/mrred85/cakephp-requests
  * @copyright 2016 - present Victor Rosu. All rights reserved.
  * @license Licensed under the MIT License.
  */
